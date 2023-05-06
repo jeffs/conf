f64c1bb642d544e67df46dfd093cab80b07e315d
Jeff Schwab <jeff@unbuggy.com>
2023-05-06 16:36:50 -0400

    Set EDITOR='code --wait' when running in VS Code

--- bin/init.zsh
+++ bin/init.zsh
@@ -5,17 +5,11 @@
 
 cd
 
-mkdir -p usr/bin .config
+mkdir -p .config
 
 # Remove existing targets (but only if they're symlinks).
-rm usr/bin/code(@) \
-    .config/nvim(@) \
-    .gitconfig(@) \
-    .tmux.conf(@) \
-    .zprofile(@) \
-    .zshrc(@)
+rm .config/nvim(@) .gitconfig(@) .tmux.conf(@) .zprofile(@) .zshrc(@)
 
-ln -s ~/conf/bin/code.zsh usr/bin/code
 ln -s ~/conf/etc/nvim .config
 ln -s ~/conf/etc/gitconfig .gitconfig
 ln -s ~/conf/etc/tmux.conf .tmux.conf
