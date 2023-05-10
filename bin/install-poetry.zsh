9cf03442d13e2ff462922b34c537339d0e79da18
Jeff Schwab <jeff@unbuggy.com>
2023-05-10 19:57:51 -0400

    Switch from pyenv to poetry
    Trying to learn it mainly because it's what Wieldy uses.

--- bin/install-poetry.zsh
+++ bin/install-poetry.zsh
@@ -1,3 +1,6 @@
 #!/usr/bin/env -S zsh -euo pipefail
 
 curl -sSL https://install.python-poetry.org | python3 -
+
+mkdir $ZSH_CUSTOM/plugins/poetry
+poetry completions zsh > $ZSH_CUSTOM/plugins/poetry/_poetry
