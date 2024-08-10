15a09397120efefac141e057cff1b40a009c3cab
Jeff Schwab <jeff@unbuggy.com>
2024-08-10 12:10:07 -0400

    Install psql

---
+++ bin/install-psql.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S zsh -euo pipefail
+#
+# Note that some tools need the libpq location in env vars.  They're set in the
+# accompanying zprofile.  See also: <https://stackoverflow.com/a/49689589/3116635>
+
+brew install libpq
