453b2cd18f59f1f227e69e3b091d0a366e1350ea
Jeff Schwab <jeff@unbuggy.com>
2024-08-12 08:49:05 -0400

    Open daily logs as VSCode projects

---
+++ bin/cl.zsh
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S zsh -euo pipefail
+#
+# Open log project for today (or --yesterday) in VSCode, creating it if it
+# doesn't already exist.
+
+if [[ $# -eq 0 ]]; then
+    readonly dir=~/log/$(date +%Y/%m/%d)
+elif [[ $1 = "--yesterday" ]]; then
+    readonly dir=~/log/$(PYTHONPATH=~/git/py-kart python -m yesterday)
+else
+    echo "$1: bad option" >&2
+    exit 2
+fi
+
+mkdir -p $dir
+cd $dir
+[ -d .git ] || git init
+
+# This flag is a terrible hack.  See also:
+# https://stackoverflow.com/questions/76987792/when-starting-vs-code-from-the-cli-can-i-make-the-workspace-trusted-without-dis
+code --disable-workspace-trust .
