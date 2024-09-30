960ae7a3ea4d26cd2162390514f48c011c20c0ab
Jeff Schwab <jeff@unbuggy.com>
2024-09-30 17:23:36 -0400

    Use nightly in evcxr_repl

--- bin/install-rust-repl.zsh
+++ bin/install-rust-repl.zsh
@@ -3,9 +3,15 @@
 # Install the EValuation ConteXt for Rust.
 
 cargo install evcxr_repl
+
+# The config directory varies by platform.
 readonly config_dir=$(evcxr <<EOF | tail -1
 :dep dirs
 println!("{}", dirs::config_dir().unwrap().display());
 EOF
 )/evcxr
-ln -s ~/conf/etc/prelude.rs $config_dir
+
+# https://github.com/evcxr/evcxr/blob/main/COMMON.md
+mkdir -p "$config_dir"
+ln -s ~/conf/etc/init.evcxr "$config_dir"
+ln -s ~/conf/etc/prelude.rs "$config_dir"
