d018e63668e12c3e318b18430633614b5ce82fc1
Jeff Schwab <jeff@unbuggy.com>
2024-10-16 17:51:54 -0400

    Add set() builder to Rust REPL prelude

--- etc/prelude.rs
+++ etc/prelude.rs
@@ -1,5 +1,12 @@
 // See also: [`evcxr/COMMON.md`](https://github.com/evcxr/evcxr/blob/main/COMMON.md)
 
+use std::collections::HashSet;
+use std::hash::Hash;
+
+fn set<T: Eq + Hash>(xs: impl IntoIterator<Item = T>) -> HashSet<T> {
+    xs.into_iter().collect()
+}
+
 fn vec<T>(xs: impl IntoIterator<Item = T>) -> Vec<T> {
     xs.into_iter().collect()
 }
--- src/update-rust.zsh
+++ src/update-rust.zsh
@@ -2,7 +2,7 @@
 
 # Uninstall and reinstall to avoid issues with corrupted component files.  I'm
 # not clear on what a component file is or how it becomes corrupted, but it bit
-# me by cause rustup update to fail thus:
+# me by causing rustup update to fail thus:
 #
 #   error: could not rename component file from
 #   '/Users/jeff/.rustup/tmp/hl9f8chal9xk6s3x_dir/bk' to
