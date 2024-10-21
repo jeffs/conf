8044fd88caa832477bb023d97429ac1c2437f644
Jeff Schwab <jeff@unbuggy.com>
2024-10-21 19:09:10 -0400

    Install Mason

--- etc/nvim/init.lua
+++ etc/nvim/init.lua
@@ -1,6 +1,23 @@
 -- https://neovim.io/doc/user/lua-guide.html
 
+vim.cmd('colorscheme elflord')
+
 vim.keymap.set({'n', 'v'}, ':', ';')
 vim.keymap.set({'n', 'v'}, ';', ':')
 vim.keymap.set({'n', 'v'}, 'q:', 'q;')
 vim.keymap.set({'n', 'v'}, 'q;', 'q:')
+
+vim.o.undofile = true
+
+require("config.lazy")
+require("mason").setup()
+
+-- require('lspconfig').rust_analyzer.setup {
+-- 	settings = {
+-- 		['rust-analyzer'] = {
+-- 			diagnostics = {
+-- 				enable = true;
+-- 			}
+-- 		}
+-- 	}
+-- }
