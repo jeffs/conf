0cb010b7f6feef20ae515306cf99312b86529c9e
Jeff Schwab <jeff@unbuggy.com>
2024-10-21 18:07:04 -0400

    Swap : and ;

---
+++ etc/nvim/init.lua
@@ -0,0 +1,6 @@
+-- https://neovim.io/doc/user/lua-guide.html
+
+vim.keymap.set({'n', 'v'}, ':', ';')
+vim.keymap.set({'n', 'v'}, ';', ':')
+vim.keymap.set({'n', 'v'}, 'q:', 'q;')
+vim.keymap.set({'n', 'v'}, 'q;', 'q:')
