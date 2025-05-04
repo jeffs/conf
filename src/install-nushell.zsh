8367a5cbc0690d9bfedbd7832c7885ff40666952
Jeff Schwab <jeff@unbuggy.com>
2025-05-04 19:42:21 -0400

    Continue configuring Nushell et al

---
+++ src/install-nushell.zsh
@@ -0,0 +1,9 @@
+#!/usr/bin/env -S zsh -euo pipefail
+
+cargo install nu
+
+# As of 2025-05-04, nufmt is strictly a placeholder project. If you actually run
+# it, it only garbles your code in ways that make it invalid. Not exaggerating:
+# It doesn't work at all.
+#
+#  cargo install --git https://github.com/nushell/nufmt
