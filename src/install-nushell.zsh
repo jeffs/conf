e7c0301cb4c6a6dc9b8c7f454a829fa8a646300e
Jeff Schwab <jeff@unbuggy.com>
2025-08-28 12:30:08 -0400

    Update existing nu_scripts in install-nushell.zsh

--- src/install-nushell.zsh
+++ src/install-nushell.zsh
@@ -1,7 +1,13 @@
 #!/usr/bin/env -S zsh -euo pipefail
 
 cargo install nu
-git clone --depth=1 https://github.com/nushell/nu_scripts ~/pkg/nu_scripts
+
+if [ ! -d ~/pkg/nu_scripts ]; then
+  git clone --depth=1 https://github.com/nushell/nu_scripts ~/pkg/nu_scripts
+else
+  cd ~/pkg/nu_scripts
+  git pull
+fi
 
 # As of 2025-05-04, nufmt is strictly a placeholder project. If you actually run
 # it, it only garbles your code in ways that make it invalid. Not exaggerating:
