89ec1885ce11ee9e24675ec20fbe851d64ef137e
Jeff Schwab <jeff@unbuggy.com>
2025-12-13 05:15:19 -0500

    Update comment

--- prj/jump/src/main.rs
+++ prj/jump/src/main.rs
@@ -1,14 +1,14 @@
 #![allow(dead_code, unused_assignments, unused_imports, unused_variables)]
 //! # Notes
 //!
-//! Reads config from `~/.config/jump/targets.csv`. The `targets.csv` file
-//! supports blank lines, comment lines (beginning with `#`), and jagged lines.
-//! The first column in each row is a directory path, and all subsequent columns
-//! are short names for that path.
+//! Reads config from `~/.config/jump/targets.csv` (where `~/.config` may be
+//! overriden via `$XDG_CONFIG_HOME`). The `targets.csv` file supports blank
+//! lines, comment lines (beginning with `#`), and jagged lines. The first
+//! column in each row is a directory path, and all subsequent columns are short
+//! names for that path.
 //!
 //! # TODO
 //!
-//! * [ ] Support database file specification at runtime, via args or env.
 //! * [ ] Support complex expansions like "yesterday's date."
 //! * [ ] Support secondary arguments, such as relative paths or query params.
 
