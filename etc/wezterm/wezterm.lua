43c39b7251bfff99dc4e556a1f8337b06f36bf5f
Jeff Schwab <jeff@unbuggy.com>
2026-01-07 14:35:25 -0500

    Make WezTerm less opaque

--- etc/wezterm/wezterm.lua
+++ etc/wezterm/wezterm.lua
@@ -63,7 +63,7 @@ config.send_composed_key_when_right_alt_is_pressed = false
 --   { event = { Down = { streak = 1, button = { WheelDown = 1 } } }, mods = 'NONE', action = wezterm.action.Nop, },
 -- }
 
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.4
 -- config.text_background_opacity = 1.0
 wezterm.on('toggle-opacity', function(window)
   local overrides = window:get_config_overrides() or {}
