06a490eacee374a98b511a7250dc9d2b9da47357
Jeff Schwab <jeff@unbuggy.com>
2026-02-27 06:51:34 -0500

    Xonsh: configure LSP; alias f

--- etc/xonsh/rc.d/config.py
+++ etc/xonsh/rc.d/config.py
@@ -1,27 +1,57 @@
 def setup():
     from pathlib import Path
-    import json
-    import subprocess
-    import sys
 
     from xonsh.built_ins import XSH
 
+    if "JEFF_LOGIN_DONE" not in XSH.env:
+        XSH.env["JEFF_LOGIN_DONE"] = True
+
+        import json
+        import sys
+
+        try:
+            env_json = Path("~/conf/var/env.json").expanduser().read_text()
+            XSH.env.update(json.loads(env_json))
+        except Exception as e:
+            print(f"error: env: {e}", file=sys.stderr)
+
     if XSH.env.get("XONSH_INTERACTIVE"):
-        XSH.aliases["c"] = "cd"
+        import os
+        import subprocess
 
-        XSH.env["PROMPT"] = (
-            "{YELLOW}{env_name}"
-            "{RESET}{BOLD_BLUE} {cwd}{branch_color}{curr_branch: {}}"
-            "{RESET} {RED}{last_return_code_if_nonzero:[{BOLD_INTENSE_RED}{}{RED}] }"
-            "{RESET}{BOLD_BLUE}{prompt_end}"
-            "{RESET} "
-        )
+        path_jj = Path("~/.cargo/bin/jj").expanduser()
+        path_jump = Path("~/conf/prj/target/release/jump").expanduser()
 
-        jj = Path("~/.cargo/bin/jj").expanduser()
+        def mc(arg):
+            dir = Path(arg).expanduser()
+            dir.mkdir(parents=True, exist_ok=True)
+            os.chdir(dir)
+
+        def alias_mc(args):
+            try:
+                [arg] = args
+            except ValueError as e:
+                return None, f"mc: {e}", 2
+            return mc(arg)
+
+        def alias_f(args):
+            try:
+                output = subprocess.run(
+                    [path_jump, *args],
+                    capture_output=True,
+                    text=True,
+                    env={k: v for k, v in XSH.env.items() if type(v) is str},
+                )
+                print(output)
+            except Exception as e:
+                return (None, str(e), 1)
+            if output.returncode == 0:
+                return mc(output.stdout)
 
         def curr_branch():
+            """TODO: Move to Rust"""
             command = [
-                jj,
+                path_jj,
                 "log",
                 "-r",
                 "heads(::@ & bookmarks())",
@@ -32,22 +62,24 @@ def setup():
             try:
                 output = subprocess.run(command, capture_output=True, text=True)
             except Exception as e:
-                print(e)
-                return None
+                return (None, str(e), 1)
             if output.returncode == 0:
                 return output.stdout.rstrip()
 
+        XSH.aliases["c"] = "cd"
+        XSH.aliases["f"] = alias_f
+        XSH.aliases["mc"] = alias_mc
+
         XSH.env["TITLE"] = "{cwd}"
         XSH.env["PROMPT_FIELDS"]["curr_branch"] = curr_branch
-
-    if "JEFF_LOGIN_DONE" not in XSH.env:
-        XSH.env["JEFF_LOGIN_DONE"] = True
-
-        try:
-            env_json = Path("~/conf/var/env.json").expanduser().read_text()
-            XSH.env.update(json.loads(env_json))
-        except Exception as e:
-            print(f"error: env: {e}", file=sys.stderr)
+        XSH.env["XONSH_SHOW_TRACEBACK"] = False
+        XSH.env["PROMPT"] = (
+            "{YELLOW}{env_name}"
+            "{RESET}{BOLD_BLUE} {cwd}{branch_color}{curr_branch: {}}"
+            "{RESET} {RED}{last_return_code_if_nonzero:[{BOLD_INTENSE_RED}{}{RED}] }"
+            "{RESET}{BOLD_BLUE}{prompt_end}"
+            "{RESET} "
+        )
 
 
 setup()
