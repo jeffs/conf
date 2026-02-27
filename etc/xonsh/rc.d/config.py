7fddcd503c1e1225970176743189df9862301355
Jeff Schwab <jeff@unbuggy.com>
2026-02-27 12:37:59 -0500

    alias y for yazi and cd

--- etc/xonsh/rc.d/config.py
+++ etc/xonsh/rc.d/config.py
@@ -1,26 +1,34 @@
+from typing import cast
+
+
 def setup():
     from pathlib import Path
+    import tempfile
 
     from xonsh.built_ins import XSH
 
-    if "JEFF_LOGIN_DONE" not in XSH.env:
-        XSH.env["JEFF_LOGIN_DONE"] = True
+    aliases = cast(dict[str, object], XSH.aliases)
+    env = cast(dict[str, object], XSH.env)
+
+    if "JEFF_LOGIN_DONE" not in env:
+        env["JEFF_LOGIN_DONE"] = True
 
         import json
         import sys
 
         try:
             env_json = Path("~/conf/var/env.json").expanduser().read_text()
-            XSH.env.update(json.loads(env_json))
+            env.update(json.loads(env_json))
         except Exception as e:
             print(f"error: env: {e}", file=sys.stderr)
 
-    if XSH.env.get("XONSH_INTERACTIVE"):
+    if env.get("XONSH_INTERACTIVE"):
         import os
         import subprocess
 
         path_jj = Path("~/.cargo/bin/jj").expanduser()
         path_jump = Path("~/conf/prj/target/release/jump").expanduser()
+        path_yazi = Path("/opt/homebrew/bin/yazi")
 
         def mc(arg):
             dir = Path(arg).expanduser()
@@ -40,7 +48,7 @@ def setup():
                     [path_jump, *args],
                     capture_output=True,
                     text=True,
-                    env={k: v for k, v in XSH.env.items() if type(v) is str},
+                    env={k: v for k, v in env.items() if type(v) is str},
                 )
                 print(output)
             except Exception as e:
@@ -66,20 +74,42 @@ def setup():
             if output.returncode == 0:
                 return output.stdout.rstrip()
 
+        # File manager; see:
+        #
+        # - <https://yazi-rs.github.io/features>.
+        # - <https://yazi-rs.github.io/docs/quick-start>
+        # def --env y [...args] {
+        # let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+        # yazi ...$args --cwd-file $tmp
+        # let cwd = (open $tmp)
+        # if $cwd != "" and $cwd != $env.PWD {
+        # cd $cwd
+        # }
+        # rm -fp $tmp
+        # }
+
+        def alias_y(args):
+            with tempfile.NamedTemporaryFile(prefix="yazi-cwd") as tmp:
+                subprocess.run([path_yazi, "--cwd-file", tmp.name])
+                s = Path(tmp.name).read_text()
+            if s:
+                os.chdir(s)
+
         # Fun fact: `...` doesn't work as an alias, because it's valid Python.
         for i in range(1, 10):
-            XSH.aliases["." * (i + 1)] = f"cd {'/'.join(['..'] * i)}"
+            aliases["." * (i + 1)] = f"cd {'/'.join(['..'] * i)}"
 
-        del XSH.aliases["ls"]
+        del aliases["ls"]
 
-        XSH.aliases["c"] = "cd"
-        XSH.aliases["f"] = alias_f
-        XSH.aliases["mc"] = alias_mc
+        aliases["c"] = "cd"
+        aliases["f"] = alias_f
+        aliases["mc"] = alias_mc
+        aliases["y"] = alias_y
 
-        XSH.env["TITLE"] = "{cwd}"
-        XSH.env["PROMPT_FIELDS"]["curr_branch"] = curr_branch
-        XSH.env["XONSH_SHOW_TRACEBACK"] = False
-        XSH.env["PROMPT"] = (
+        env["TITLE"] = "{cwd}"
+        cast(dict, env["PROMPT_FIELDS"])["curr_branch"] = curr_branch
+        env["XONSH_SHOW_TRACEBACK"] = False
+        env["PROMPT"] = (
             "{YELLOW}{env_name}"
             "{RESET}{BOLD_BLUE} {cwd}{branch_color}{curr_branch: {}}"
             "{RESET} {RED}{last_return_code_if_nonzero:[{BOLD_INTENSE_RED}{}{RED}] }"
