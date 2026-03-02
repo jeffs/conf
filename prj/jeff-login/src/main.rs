b5f67299c9d4dcc3ad77c3aa9f80d588f80e45cc
Jeff Schwab <jeff@unbuggy.com>
2026-03-02 10:41:38 -0500

    Manual cleanup

--- prj/jeff-login/src/main.rs
+++ prj/jeff-login/src/main.rs
@@ -1,19 +1,6 @@
 //! Login shell launcher. See [`main`] for notes.
 
-use std::{ffi, fs, io, path::Path};
-
-/// TODO: Replace with [`std::env::join_paths`].
-fn path_join(dirs: &[&Path]) -> ffi::OsString {
-    let mut dirs = dirs.iter();
-    let Some(first) = dirs.next() else {
-        return ffi::OsString::new();
-    };
-    dirs.fold(first.as_os_str().to_os_string(), |mut acc, dir| {
-        acc.push(":");
-        acc.push(dir);
-        acc
-    })
-}
+use std::{env, error::Error, ffi, fs, io, path::Path};
 
 /// Writes the specified environment to a JSON object in the specified file.
 ///
@@ -45,7 +32,7 @@ fn write_json<'a>(
     fs::write(dest, json)
 }
 
-fn escape_sh(s: &ffi::OsStr, out: &mut Vec<u8>) {
+fn write_sh_escaped(out: &mut Vec<u8>, s: &ffi::OsStr) {
     for &b in s.as_encoded_bytes() {
         if b == b'\'' {
             out.extend_from_slice(br"'\''");
@@ -55,50 +42,47 @@ fn escape_sh(s: &ffi::OsStr, out: &mut Vec<u8>) {
     }
 }
 
-fn write_sh_var(key: &ffi::OsStr, value: &ffi::OsStr, out: &mut Vec<u8>) {
+fn write_sh_var(out: &mut Vec<u8>, key: &ffi::OsStr, value: &ffi::OsStr) {
     out.extend_from_slice(b"export ");
     out.extend_from_slice(key.as_encoded_bytes());
     out.extend_from_slice(b"='");
-    escape_sh(value, out);
+    write_sh_escaped(out, value);
     out.extend_from_slice(b"'\n");
 }
 
 /// Writes the specified environment to a POSIX shell script.
 fn write_sh<'a>(
+    dest: &Path,
     env: impl IntoIterator<Item = (&'a String, &'a String)>,
     path: &[&Path],
-    dest: &Path,
-) -> io::Result<()> {
+) -> Result<(), Box<dyn Error>> {
     let mut out = b"# This file is generated. See ~/conf/prj/jeff-login.\n\n".to_vec();
     for (key, value) in env {
-        write_sh_var(key.as_ref(), value.as_ref(), &mut out);
+        write_sh_var(&mut out, key.as_ref(), value.as_ref());
     }
-    write_sh_var(ffi::OsStr::new("PATH"), &path_join(path), &mut out);
-    fs::write(dest, out)
+    write_sh_var(&mut out, ffi::OsStr::new("PATH"), &env::join_paths(path)?);
+    Ok(fs::write(dest, out)?)
 }
 
 /// # Panics
 ///
 /// Will panic on file output errors or if the platform config cannot be loaded.
 fn main() {
-    #[allow(deprecated)]
+    // Load OS-specific values.
     let home = std::env::home_dir().expect("home dir");
-    let conf_root = home.join("conf");
-
-    let platform = platform::Platform::load(&conf_root).expect("platform config: {e}");
+    let conf = home.join("conf");
+    let platform = platform::Platform::load(&conf).expect("loading platform config");
     let path = platform.full_path();
 
     // Make sure the destination directory exists.
-    let var_dir = conf_root.join("var");
-    fs::create_dir_all(&var_dir).unwrap_or_else(|e| panic!("{}: {e}", var_dir.display()));
+    let var = conf.join("var");
+    fs::create_dir_all(&var).unwrap_or_else(|e| panic!("{}: {e}", var.display()));
 
     // Save JSON for Nushell and Xonsh.
-    let json_file = var_dir.join("env.json");
-    write_json(&platform.env, &path, &json_file)
-        .unwrap_or_else(|e| panic!("{}: {e}", json_file.display()));
+    let json = var.join("env.json");
+    write_json(&platform.env, &path, &json).unwrap_or_else(|e| panic!("{}: {e}", json.display()));
 
     // Save exports for POSIX shells.
-    let sh_file = var_dir.join("env.sh");
-    write_sh(&platform.env, &path, &sh_file)
-        .unwrap_or_else(|e| panic!("{}: {e}", sh_file.display()));
+    let sh = var.join("env.sh");
+    write_sh(&sh, &platform.env, &path).unwrap_or_else(|e| panic!("{}: {e}", sh.display()));
 }
