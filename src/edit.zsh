ac5dad802c8d4ed8ea217deb036645ff8670fcb2
Jeff Schwab <jeff@unbuggy.com>
2026-03-22 04:46:44 -0400

    Helix: Fix runtime dir path

--- src/edit.zsh
+++ src/edit.zsh
@@ -3,4 +3,4 @@
 # Invoke my preferred editing environment from outside an existing Nushell.
 # Mainly for use as my Zellij `scrollback_editor`.
 
-HELIX_RUNTIME=~/pkg/helix/runtime ~/.cargo/bin/hx "$@"
+HELIX_RUNTIME=~/usr/src/helix/runtime ~/.cargo/bin/hx "$@"
