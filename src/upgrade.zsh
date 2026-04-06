39428b975a6444a4e7a0a17c68c90b8a8e632d15
Jeff Schwab <jeff@unbuggy.com>
2026-04-06 11:16:25 -0400

    Link upgrade script; have it call Rust

--- src/upgrade.zsh
+++ src/upgrade.zsh
@@ -2,55 +2,10 @@
 #
 # TODO
 #
-# - [ ] Upgrade:
-#   + forked packages, such as evcxr_repl
-#   + apps from installers: Docker, Firefox, Slack, Steam
-#   + Docker images
-# - [ ] Install:
-#   + projects; e.g., `../prj/jump`
-#   + apps; e.g., `../app/on-file-click.app`
+# - [ ] Upgrade apps from installers: Docker, Firefox, Slack, Steam
+# - [ ] Update Docker images
+# - [ ] Build `on-file-click.app`
+# - [ ] Consolidate with `cargo run -p sync`
 
-# Prompt for SSH passphrase up front if no key is loaded, so this script
-# doesn't pause for a passphrase half way through.
-if ! ssh-add -l >/dev/null 2>&1; then
-    eval "$(ssh-agent -s)"
-    ssh-add
-fi
-
-# Upgrade Homebrew packages.
-echo '* Homebrew'
-echo '** brew upgrade --quiet'
-brew upgrade --quiet
-
-# Upgrade Rust and its tooling. The `rustup update` command is occasionally
-# bemused by some kind of cache corruption.  Should that occur, run
-# `update-rust.zsh`.
-echo '* Rustup'
-echo '** rustup update'
-rustup update
-
-# Update all crates that were not installed from local filesystem paths.  The
-# output of `cargo install --list` includes such paths in parentheses, so it's
-# not hard to grep them out.
-echo '* Cargo'
-readonly crates=$(cargo install --list | awk '/^[^ ][^(]*$/ { print $1 }' | tr '\n' ' ')
-echo "** cargo install $crates"
-cargo install ${(s: :)crates}
-
-~/conf/src/install-nushell.zsh
-~/conf/src/install-helix.nu
-
-# Run this occasionally to avoid undue bitrot. But GOOD LORD this is slow.
-# ~/conf/src/install-uv.zsh
-
-uv tool install specify-cli --force --from git+https://github.com/github/spec-kit.git
-
-# Passing --install --all rather than --list would cause sudo to prompt for
-# access, but this script is not meant to be interactive.
-echo '* Software Update Tool'
-echo '** softwareupdate --list'
-
-# brew install expect for unbuffer (or use prj/upgrade instead of upgrade.zsh).
-#
-# unbuffer softwareupdate --list \
-#   | rg -v '^(Software Update Tool|Finding available software|No new software available\.|)$'
+cd ~/conf/prj
+cargo run -p upgrade
