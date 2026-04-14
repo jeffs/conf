737e84bbc2f736d16a7b15bcc12b5f53ad7c8f5d
Jeff Schwab <jeff@unbuggy.com>
2026-04-14 11:47:54 -0400

    claude: Stop begging me to integrate Google
    This commit also automates a check for pathtree in watch-jj.

--- src/watch-jj.sh
+++ src/watch-jj.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
 
-viddy -n 1 "
-  2>&1 jj diff --summary | 2>&1 pathtree --color=always
-  2>&1 jj --color=always
-"
-
-# Or, without pathtree (which is from rust-kart):
-#
-# viddy -n 1 "
-#   2>&1 jj --color=always diff --summary
-#   2>&1 jj --color=always
-# "
+# Pathtree is available at <https://github.com/jeffs/pathtree>.
+# The absolute path is used here to avoid name collisions (and a PATH search).
+if [ -x ~/.cargo/bin/pathtree ]; then
+  viddy -n 1 "
+    2>&1 jj diff --summary | 2>&1 pathtree --color=always
+    2>&1 jj --color=always
+  "
+else
+  viddy -n 1 "
+    2>&1 jj --color=always diff --summary
+    2>&1 jj --color=always
+  "
+fi
