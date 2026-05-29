707d98abd35c8dbb0fc7e81d46298b673e318562
Jeff Schwab <jeff@unbuggy.com>
2026-05-29 11:51:52 -0400

    watch-jj: Use explicit path for pathtree

--- src/watch-jj.sh
+++ src/watch-jj.sh
@@ -4,7 +4,7 @@
 # The absolute path is used here to avoid name collisions (and a PATH search).
 if [ -x ~/.cargo/bin/pathtree ]; then
   viddy -n 1 "
-    2>&1 jj diff --summary | 2>&1 pathtree --color=always
+    2>&1 jj diff --summary | 2>&1 ~/.cargo/bin/pathtree --color=always
     2>&1 jj --color=always
   "
 else
