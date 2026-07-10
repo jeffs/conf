e889c87f8ed5fd2b94aa6e9c864d046fb742353c
Jeff Schwab <jeff@unbuggy.com>
2026-07-10 10:10:28 -0400

    watch-jj: Omit timestamps from log output

--- src/watch-jj.sh
+++ src/watch-jj.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+# Omitting timestamps keeps `jj log` output comfortably narrow.
+readonly no_time='template-aliases."format_timestamp(timestamp)"='
+
 # Pathtree is available at <https://github.com/jeffs/pathtree>.
 # The absolute path is used here to avoid name collisions (and a PATH search).
 if [ -x ~/.cargo/bin/pathtree ]; then
   viddy -n 1 "
     2>&1 jj diff --summary | 2>&1 ~/.cargo/bin/pathtree --color=always
-    2>&1 jj --color=always
+    2>&1 jj --color=always --config '$no_time'
   "
 else
   viddy -n 1 "
     2>&1 jj --color=always diff --summary
-    2>&1 jj --color=always
+    2>&1 jj --color=always --config '$no_time'
   "
 fi
