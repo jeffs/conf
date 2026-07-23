629db982902025b1bf73fc659c4a5f0d27fd8fce
Jeff Schwab <jeff@unbuggy.com>
2026-07-23 13:48:22 -0400

    Doc: Correct comment in reference doc

--- doc/ref/rage.sh
+++ doc/ref/rage.sh
@@ -2,7 +2,7 @@
 cargo install --locked rage
 
 # Encrypt
-env PINENTRY_PROGRAM= rage -d file.age | tar zx
+env PINENTRY_PROGRAM= rage --passphrase -o file.age file
 
 # Decrypt
-env PINENTRY_PROGRAM= rage --passphrase -o file.age file
+env PINENTRY_PROGRAM= rage -d file.age | tar zx
